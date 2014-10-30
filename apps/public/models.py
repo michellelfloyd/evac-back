4d063f07520e800c3b8b93434cac0592251edd71
Bob Ziroll <elderziroll@gmail.com>
2014-10-30 17:55:55 -0600

    Fixed a merge conflict problem in models.py

--- apps/public/models.py
+++ apps/public/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-<<<<<<< Updated upstream
 # class User(models.Model):
 #     name = models.CharField(max_length=100)
 #     email = models.EmailField(max_length=254)
